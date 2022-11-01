06560869a63d68460f3e40d4fa5a21b112d00652
Md. Shahadot Hossain <shahadotice@gmail.com>
2022-11-01 12:23:10 +0600

    Implementing custom formetter

--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -18,12 +18,14 @@ builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
+
 //builder.Services.AddControllers();
 builder.Services.AddControllers(config => {
-    config.RespectBrowserAcceptHeader = true;
-    config.ReturnHttpNotAcceptable = true;
+   config.RespectBrowserAcceptHeader = true;
+   config.ReturnHttpNotAcceptable = true;
 }).AddXmlDataContractSerializerFormatters()
-.AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+  .AddCustomCSVFormatter()
+  .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 
 
